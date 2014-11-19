ce755e702793910c64660be5d6db2a90dbedd095
epitomz <antonin.delbos@gmail.com>
2014-11-19 14:06:23 +0100

    action produit

--- src/action/ProduitAction.java
+++ src/action/ProduitAction.java
@@ -2,13 +2,62 @@ package action;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import metier.Produit;
 
 import com.opensymphony.xwork2.ActionSupport;
+
+import dao.IBoutiqueDao;
 
 public class ProduitAction extends ActionSupport {
 
 	private Produit produit;
 	private List<Produit> produits;
+	
+	@Autowired
+	public IBoutiqueDao dao;
+	
+	public String liste() {
+		return SUCCESS;
+	}
+	
+	public String save() {
+		return SUCCESS;
+	}
+	
+	public String update() {
+		return SUCCESS;
+	}
+	
+	public String delete() {
+		return SUCCESS;
+	}
+
+	public Produit getProduit() {
+		return produit;
+	}
+
+	public void setProduit(Produit produit) {
+		this.produit = produit;
+	}
+
+	public List<Produit> getProduits() {
+		return produits;
+	}
+
+	public void setProduits(List<Produit> produits) {
+		this.produits = produits;
+	}
+
+	public IBoutiqueDao getDao() {
+		return dao;
+	}
+
+	public void setDao(IBoutiqueDao dao) {
+		this.dao = dao;
+	}
+	
+	
 	
 }
