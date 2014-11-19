06e18f35b5a044af5d3865501b796bb4b22a0063
epitomz <antonin.delbos@gmail.com>
2014-11-19 14:02:12 +0100

    localhost

--- src/action/ProduitAction.java
+++ src/action/ProduitAction.java
@@ -1,5 +1,14 @@
 package action;
 
-public class ProduitAction {
+import java.util.List;
 
+import metier.Produit;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class ProduitAction extends ActionSupport {
+
+	private Produit produit;
+	private List<Produit> produits;
+	
 }
