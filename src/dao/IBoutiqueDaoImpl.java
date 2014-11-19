36f3137b3142c01836a0f403370da6af53ed161b
mihawk33 <pierresoares@live.fr>
2014-11-19 12:11:40 +0100

    mise à jour dao
    import

--- src/dao/IBoutiqueDaoImpl.java
+++ src/dao/IBoutiqueDaoImpl.java
@@ -15,9 +15,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import business.model.City;
-
-import com.sun.security.ntlm.Client;
 
 @Repository("BoutiqueDao")
 @Transactional
