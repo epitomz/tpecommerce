881f79fc7d612df4f72467ab8676a72e5f32334d
shura33 <adrien.magnat@laposte.net>
2014-11-19 14:07:10 +0100

    ajout d'une classe action et d'un jsp pour des tests

---
+++ src/action/HelloClass.java
@@ -0,0 +1,55 @@
+package action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class HelloClass extends ActionSupport {
+
+	private String message;
+	private String nom;
+	
+	
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public HelloClass() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String execute() throws Exception {
+		// on mets à jour les variables acessibles après par les getter/setter
+		setMessage("Struts est lancé");
+		return SUCCESS;
+	}
+	
+	public String bonjour(){
+		setMessage("Bonjour Bob");
+		return SUCCESS;
+	}
+	
+	public String versFormulaire(){
+		setMessage("Bienvenue dans le formulaire");
+		return SUCCESS;
+	}
+	
+	public String infosFormulaire(){
+		
+		setMessage(getNom());
+		return SUCCESS;
+	}
+
+}
