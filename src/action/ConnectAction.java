b91b14f664c0951361496689627da46850a49499
epitomz <antonin.delbos@gmail.com>
2014-11-20 11:04:47 +0100

    struts

--- src/action/ConnectAction.java
+++ src/action/ConnectAction.java
@@ -1,10 +1,26 @@
 package action;
 
+import metier.User;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import service.IAdminCategoriesService;
+
 import com.opensymphony.xwork2.ActionSupport;
 
 public class ConnectAction extends ActionSupport {
+	
+	@Autowired
+	public IAdminCategoriesService AdminCategoriesService;
+	
+	private User user;
 
 	public String login() {
+		return SUCCESS;
+	}
+
+	public String connect() {
+		AdminCategoriesService.Authentifier(user);
 		return SUCCESS;
 	}
 }
