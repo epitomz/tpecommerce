64ddba1a755d82ebe84419789428f2f8081a7c38
shura33 <adrien.magnat@laposte.net>
2014-11-20 11:17:23 +0100

    ptite modif delete

--- src/action/ProduitAction.java
+++ src/action/ProduitAction.java
@@ -64,6 +64,7 @@ public class ProduitAction extends ActionSupport {
 	public String deleteP() {
 		AdminCategoriesService.supprimerProduit(ref);
 		this.listeCetP();
+		produit = new Produit();
 		return SUCCESS;
 	}
 
