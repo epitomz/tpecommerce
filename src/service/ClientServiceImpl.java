f6f53bef3a8a66b6206daa628017114aea9274e9
mihawk33 <pierresoares@live.fr>
2014-11-20 14:42:49 +0100

    interface clientimpl

--- src/service/ClientServiceImpl.java
+++ src/service/ClientServiceImpl.java
@@ -1,10 +1,12 @@
 package service;
 
 import java.util.List;
+import java.util.Set;
 
 import metier.Categorie;
 import metier.Client;
 import metier.Commande;
+import metier.LigneCommande;
 import metier.Panier;
 import metier.Produit;
 import metier.User;
@@ -21,11 +23,105 @@ public class ClientServiceImpl implements IClientService{
 	@Autowired
 	private IBoutiqueDao dao;
 
+	//Commande----------------------------------------------------------------------
 	@Override
 	public Long saveCommande(Commande c) {
 		// TODO Auto-generated method stub
 		return dao.saveCommande(c);
 	}
+
+	@Override
+	public void deleteCommande(Long idCommande) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public Commande getCommande(Long idCommande) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public void updateCommande(Commande c) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	//LigneCommande------------------------------------------------------------------
+	@Override
+	public Long saveLigneCommande(LigneCommande l) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public void deleteLigneCommande(Long idLigneCommande) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void updateLigneCommande(LigneCommande l) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public LigneCommande getLigneCommande(Long idLigneCommande) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public Set<LigneCommande> listerLigneCommande() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public Set<LigneCommande> listerLigneCommande(Long idClient) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	//Client--------------------------------------------------------------------------
+
+	@Override
+	public Long saveClient(Client c) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public void deleteClient(Long idClient) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void updateClient(Client c) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public Client getClient(Long idClient) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	//Internaute--------------------------------------------------------------------
+
 
 	@Override
 	public String Authentifier(User user) {
--- src/service/IClientService.java
+++ src/service/IClientService.java
@@ -1,10 +1,32 @@
 package service;
 
+import java.util.Set;
+
+import metier.Client;
 import metier.Commande;
+import metier.LigneCommande;
 
 public interface IClientService extends InternauteBoutiqueService {
 	
-	public Long saveCommande(Commande c);
+	    //Commande---------------
+		public Long saveCommande(Commande c);
+		public void deleteCommande(Long idCommande);
+		public Commande getCommande(Long idCommande);
+		public void updateCommande(Commande c);
+		
+		//LigneCommande---
+		public Long saveLigneCommande(LigneCommande l);
+		public void deleteLigneCommande(Long idLigneCommande);
+		public void updateLigneCommande(LigneCommande l);
+		public LigneCommande getLigneCommande(Long idLigneCommande);
+		public Set<LigneCommande> listerLigneCommande();
+		public Set<LigneCommande> listerLigneCommande(Long idClient);
+		
+		//Client-----------------
+		public Long saveClient(Client c);
+		public void deleteClient(Long idClient);
+		public void updateClient(Client c);
+		public Client getClient(Long idClient);
 	
 	
 
