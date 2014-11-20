25dc8a08e5fd6a8dad4cb4aeef86e3f84c2d7c6d
mihawk33 <pierresoares@live.fr>
2014-11-20 13:36:37 +0100

    Iclientservice

--- src/service/IClientService.java
+++ src/service/IClientService.java
@@ -3,8 +3,6 @@ package service;
 import metier.Produit;
 
 public interface IClientService extends InternauteBoutiqueService {
-	public Long ajouterProduit(Produit p, Long idCat);
-	public void supprimerProduit(Long idP);
-	public void modifierProduit(Produit p);
+	
 
 }
